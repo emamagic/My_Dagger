cd6c0f7ee0cd32433390fa86fdfd5c329d7ebfd7
emamagic <developermagic7@gmail.com>
2021-12-16 10:41:02 +0330

    benefit of declared modules as abstract

--- app/src/main/java/com/emamagic/my_dagger/dagger/WheelsModules.kt
+++ app/src/main/java/com/emamagic/my_dagger/dagger/WheelsModules.kt
@@ -6,30 +6,59 @@ import com.emamagic.my_dagger.car.Wheels
 import dagger.Module
 import dagger.Provides
 
+// we declared as abstract because dagger do not create instance of WheelsModules
 @Module
-object WheelsModules {
+abstract class WheelsModules {
 
-    @JvmStatic
-    @Provides
-    fun provideRims(): Rims {
-        return Rims()
+    companion object {
+
+        @JvmStatic
+        @Provides
+        fun provideRims(): Rims {
+            return Rims()
+        }
+
+
+        @JvmStatic
+        @Provides
+        fun provideTires(): Tires {
+            val tires = Tires()
+            tires.inflate()
+            return tires
+        }
+
+        @JvmStatic
+        @Provides
+        fun provideWheels(tires: Tires, rims: Rims): Wheels {
+            return Wheels(rims, tires)
+        }
+
     }
-
-
-    @JvmStatic
-    @Provides
-    fun provideTires(): Tires {
-        val tires = Tires()
-        tires.inflate()
-        return tires
-    }
-
-    @JvmStatic
-    @Provides
-    fun provideWheels(tires: Tires, rims: Rims): Wheels {
-        return Wheels(rims, tires)
-    }
-
-
 
 }
+
+//@Module
+//object WheelsModules {
+//
+//    @JvmStatic
+//    @Provides
+//    fun provideRims(): Rims {
+//        return Rims()
+//    }
+//
+//
+//    @JvmStatic
+//    @Provides
+//    fun provideTires(): Tires {
+//        val tires = Tires()
+//        tires.inflate()
+//        return tires
+//    }
+//
+//    @JvmStatic
+//    @Provides
+//    fun provideWheels(tires: Tires, rims: Rims): Wheels {
+//        return Wheels(rims, tires)
+//    }
+//
+//}
